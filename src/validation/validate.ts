51abf0988afd3be4bab27464b9ced87ae7622570
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-07-26 21:29:55 +0300

    validate: remove warning about throwing literal (#3680)

--- src/validation/validate.ts
+++ src/validation/validate.ts
@@ -57,7 +57,9 @@ export function validate(
   // If the schema used for validation is invalid, throw an error.
   assertValidSchema(schema);
 
-  const abortObj = Object.freeze({});
+  const abortError = new GraphQLError(
+    'Too many validation errors, error limit reached. Validation aborted.',
+  );
   const errors: Array<GraphQLError> = [];
   const context = new ValidationContext(
     schema,
@@ -65,13 +67,7 @@ export function validate(
     typeInfo,
     (error) => {
       if (errors.length >= maxErrors) {
-        errors.push(
-          new GraphQLError(
-            'Too many validation errors, error limit reached. Validation aborted.',
-          ),
-        );
-        // eslint-disable-next-line @typescript-eslint/no-throw-literal
-        throw abortObj;
+        throw abortError;
       }
       errors.push(error);
     },
@@ -88,8 +84,10 @@ export function validate(
       visitWithTypeInfo(typeInfo, visitor),
       QueryDocumentKeysToValidate,
     );
-  } catch (e) {
-    if (e !== abortObj) {
+  } catch (e: unknown) {
+    if (e === abortError) {
+      errors.push(abortError);
+    } else {
       throw e;
     }
   }
