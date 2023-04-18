94763089835bf3f30714f167e297d93f8be4601d
Tom Gasson <tom@gasson.com.au>
2023-04-18 21:11:02 +1000

    Expose printDirective function to enable schema sharding (#3822)

--- src/utilities/printSchema.ts
+++ src/utilities/printSchema.ts
@@ -268,7 +268,7 @@ function printInputValue(arg: GraphQLInputField): string {
   return argDecl + printDeprecated(arg.deprecationReason);
 }
 
-function printDirective(directive: GraphQLDirective): string {
+export function printDirective(directive: GraphQLDirective): string {
   return (
     printDescription(directive) +
     'directive @' +
