e5967f5e581e9ff00e79187f1834748806182b8d
Yaacov Rydzinski <yaacovCR@gmail.com>
2024-10-13 19:04:18 +0300

    improve new leaf type `parseConstLiteral()` method (#4218)
    the new method was introduced in #3812 with upgrade path improved in
    #4209.
    
    This PR completes polishes this work a bit:
    
    1. The method is renamed to `coerceInputLiteral()` following the naming
    convention suggested in #2357, with the other methods to be renamed in
    later PRs.
    2. The type of the method `GraphQLScalarInputLiteralCoercer` is
    exported, which was not done in the initial work. The old
    `GraphQLScalarLiteralParser` type is deprecated.

--- src/index.ts
+++ src/index.ts
@@ -196,7 +196,9 @@ export type {
   GraphQLUnionTypeExtensions,
   GraphQLScalarSerializer,
   GraphQLScalarValueParser,
+  /* @deprecated in favor of GraphQLScalarInputLiteralCoercer, will be removed in v18 */
   GraphQLScalarLiteralParser,
+  GraphQLScalarInputLiteralCoercer,
   GraphQLDefaultValueUsage,
 } from './type/index.js';
 
