06fae0dc4310597fad576528a66555b17a92e6a4
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-22 09:02:47 -0500

    refactor: treeshakable kind enum (#4270)
    closes https://github.com/graphql/graphql-js/issues/4253

--- src/index.ts
+++ src/index.ts
@@ -204,6 +204,9 @@ export type {
 } from './type/index.js';
 
 // Parse and operate on GraphQL language source files.
+// @see https://github.com/typescript-eslint/typescript-eslint/issues/10313
+// eslint-disable-next-line @typescript-eslint/consistent-type-exports
+export { Kind } from './language/kinds.js';
 export {
   Token,
   Source,
@@ -229,7 +232,6 @@ export {
   visitInParallel,
   getEnterLeaveForKind,
   BREAK,
-  Kind,
   DirectiveLocation,
   // Predicates
   isDefinitionNode,
