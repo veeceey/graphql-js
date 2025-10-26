a2158cfdc15e0a2dc437b106f047d89f4702d9c0
Yaacov Rydzinski <yaacovCR@gmail.com>
2025-10-26 12:37:35 +0200

    refactor(execution): extract withCleanup from mapAsyncIterable (#4496)
    motivations:
    
    1. separation of concerns (`mapAsyncIterable` should have single
    function)
    2. `withCleanUp` is useful independently, i.e. it removes the need to
    "hand-roll" AsyncGenerators across the codebase
    
    Some background for Point 2:
    
    Built in async generators have a feature/quirk/annoyance that all of
    their calls are sequential. If a call to `gen.next()` is pending, i.e.
    waiting on the return of a promise (`... const result = await
    fetchExternalData; yield result ... `), then the consumer cannot call
    `gen.return()` manually to abort the generator, the call to
    `gen.return()` will not initiate until the external fetch is awaited.
    This is because Async Generators (and promises more broadly) do not have
    cancellation as a built-in-feature.
    
    Hand-rolled Async Generators can allow a call to `gen.return()` to be
    used to abort the generator; they can start immediately. Technically,
    they can even return out of order with that pending `gen.next()`,
    although it's helpful if they always return in order, like with
    Repeaters (https://repeater.js.org/).
    
    We were previously using "hand-rolled" Async Generators for exactly this
    purpose. With this change, only `withCleanUp` and `mapAsyncIterable`
    stay as "hand-rolled" Async Generators, but we can use actual Async
    Generators in our actual incremental delivery code (within `Incremental
    Publisher` ) and subscription test code (`simplePubSub`).
    
    This is good for two reasons:
    
    A. Using JavaScript primitives is more modern/ergonomic
    B. Hopefully will reduce churn and bug surface.
    
    In terms of `B`, this PR also fixes a potential bug introduced when we
    upgraded TS to 5.2. That version of TypeScript requires "hand-rolled"
    Async Generators to include an `[Symbol.asyncDispose]` property method,
    but the runtime is responsible for supplying the functionality polyfill.
    When we upgraded, we by accident required our users to have that
    functionality (which is present in all of our tested versions) but not
    guaranteed. This change updates `mapAsyncIterable` and `withCleanUp` to
    fall back to `Symbol.for("asyncDispose")` which should avoid a crash.
    The broader codebase should not have to be aware of this level of detail
    when using these helpers!

--- src/execution/execute.ts
+++ src/execution/execute.ts
@@ -66,6 +66,7 @@ import { getVariableSignature } from './getVariableSignature.js';
 import { mapAsyncIterable } from './mapAsyncIterable.js';
 import type { VariableValues } from './values.js';
 import { getArgumentValues, getVariableValues } from './values.js';
+import { withCleanup } from './withCleanup.js';
 
 /* eslint-disable max-params */
 // This file contains a lot of such errors but we plan to refactor it anyway
@@ -467,10 +468,7 @@ export function validateExecutionArgs(
     schema,
     variableDefinitions,
     rawVariableValues ?? {},
-    {
-      maxErrors: options?.maxCoercionErrors ?? 50,
-      hideSuggestions,
-    },
+    { maxErrors: options?.maxCoercionErrors ?? 50, hideSuggestions },
   );
 
   if (variableValuesOrErrors.errors) {
@@ -1561,19 +1559,23 @@ function mapSourceToResponse(
   // GraphQL `execute` function, with `payload` as the rootValue.
   // This implements the "MapSourceToResponseEvent" algorithm described in
   // the GraphQL specification..
-  return mapAsyncIterable(
-    abortSignalListener
-      ? cancellableIterable(resultOrStream, abortSignalListener)
-      : resultOrStream,
-    (payload: unknown) => {
-      const perEventExecutionArgs: ValidatedExecutionArgs = {
-        ...validatedExecutionArgs,
-        rootValue: payload,
-      };
-      return validatedExecutionArgs.perEventExecutor(perEventExecutionArgs);
-    },
-    () => abortSignalListener?.disconnect(),
-  );
+  function mapFn(payload: unknown): PromiseOrValue<ExecutionResult> {
+    const perEventExecutionArgs: ValidatedExecutionArgs = {
+      ...validatedExecutionArgs,
+      rootValue: payload,
+    };
+    return validatedExecutionArgs.perEventExecutor(perEventExecutionArgs);
+  }
+
+  return abortSignalListener
+    ? withCleanup(
+        mapAsyncIterable(
+          cancellableIterable(resultOrStream, abortSignalListener),
+          mapFn,
+        ),
+        () => abortSignalListener.disconnect(),
+      )
+    : mapAsyncIterable(resultOrStream, mapFn);
 }
 
 export function executeSubscriptionEvent(
